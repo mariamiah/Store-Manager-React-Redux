c8c40f3ab13035059b94cc033b0215e6fff134c5
mariah <mariah.kakande@gmail.com>
2019-02-11 22:27:56 +0300

    chore(hound): configure hound for the project - modify webpack config file [Delivers #163866592]

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'dist/'),
+    path: path.join(__dirname, '/dist'),
     filename: 'index_bundle.js',
   },
   module: {
