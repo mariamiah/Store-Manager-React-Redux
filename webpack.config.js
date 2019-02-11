3c9de29fcc599a0cdd28922cc0c3568ee8351a94
mariah <mariah.kakande@gmail.com>
2019-02-11 17:24:41 +0300

    ch(setup): setup empty react project [Delivers #163866414]

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,34 @@ const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    output:{
-        path: path.join(__dirname, 'dist/'),
-        filename: "index_bundle.js"
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist/'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
     },
-    module:{
-        rules:[{
-            test:/\.js$/,
-            exclude:/node_modules/,
-            use:{
-                loader: "babel-loader"
-            }
-        },
     {
-        test:/\.css$/,
-        loaders:[
-            "style-loader","css-loader"
-        ]
+      test: /\.css$/,
+      loaders: [
+        'style-loader', 'css-loader',
+      ],
     },
-{
-        test:/\.scss$/,
-        loaders:["style-loader", "css-loader", "sass-loader"]
-}]
-    },
-    plugins:[
-        new HtmlPlugin({template: "./src/index.html"})
-    ]
-}
-
-    
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    }],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlPlugin({ template: './src/index.html' }),
+  ],
+};
