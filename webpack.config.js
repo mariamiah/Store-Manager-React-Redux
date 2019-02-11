76d0cedf1d2375e4bc8c701cdec6158e8fd7c571
mariah <mariah.kakande@gmail.com>
2019-02-11 17:24:41 +0300

    ch(setup): setup empty react project [Delivers #163866414]

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output:{
+        path: path.join(__dirname, 'dist/'),
+        filename: "index_bundle.js"
+    },
+    module:{
+        rules:[{
+            test:/\.js$/,
+            exclude:/node_modules/,
+            use:{
+                loader: "babel-loader"
+            }
+        },
+    {
+        test:/\.css$/,
+        loaders:[
+            "style-loader","css-loader"
+        ]
+    },
+{
+        test:/\.scss$/,
+        loaders:["style-loader", "css-loader", "sass-loader"]
+}]
+    },
+    plugins:[
+        new HtmlPlugin({template: "./src/index.html"})
+    ]
+}
+
+    
