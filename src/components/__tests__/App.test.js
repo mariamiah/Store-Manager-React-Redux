933f599037136429ff346ba291f56103a616f051
mariah <mariah.kakande@gmail.com>
2019-02-12 14:12:10 +0300

    chore(test): configure test environment for the application [Starts #163882571]

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from '../App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
