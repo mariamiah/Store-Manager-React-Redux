4483e179166d16701e6f25087508fd808706e537
mariah <mariah.kakande@gmail.com>
2019-02-25 14:01:44 +0300

    chore(sass): setup sass processing on the client [Delivers #164194245]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes/index';
+import './assets/style.scss';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
