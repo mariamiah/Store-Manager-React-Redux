f5346337854f3041c026bd9fdc70ef538ad46cec
mariah <mariah.kakande@gmail.com>
2019-03-04 18:41:15 +0300

    feat(landingpage): add landing page [Delivers #164323487]

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './Store';
 import Routes from './routes/index';
-import './assets/style.scss';
 import './assets/css/style.css';
 
 ReactDOM.render(
