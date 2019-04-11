f21e27413d13ebc4811ace91cdbd0fca62269711
mariah <mariah.kakande@gmail.com>
2019-04-11 18:02:47 +0300

    modify store another file

--- src/Store.js
+++ src/Store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
+
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk)),
